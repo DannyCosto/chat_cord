b8d2759044a55e20197197b743241957273064f9
Danny <danny.costelloe@gmail.com>
2021-07-02 14:38:51 -0400

    feature freeze

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/messages')
+const {userJoin, getCurrentUser, userLeave, getRoomUsers} = require('./utils/users')
 
 const app = express();
 const server = http.createServer(app)
@@ -17,26 +18,47 @@ const botName = 'ChatCord Bot'
 io.on('connection', socket =>{
 
     socket.on('joinRoom',({username, room})=>{
-        
+        const user = userJoin(socket.id, username,room)
+
+        socket.join(user.room);
+
 
             //sends welcome message to individual client
-    socket.emit('message',formatMessage(botName, 'Welcome to ChatCord!'));
+        socket.emit('message',formatMessage(botName, 'Welcome to ChatCord!'));
 
     //Broadcast when a user connects, except to indiviual client
-    socket.broadcast.emit('message',formatMessage(botName, 'A user has joined the chat'));
+        socket.broadcast.to(user.room).emit('message',formatMessage(botName, `${user.username} has joined the chat`));
+
+            //Send users and room info
+        io.to(user.room).emit('roomUsers',{
+        room: user.room,
+        users: getRoomUsers(user.room)
+        });
+    });
 
 
-    })
 
     //listen for chatMessage
     socket.on('chatMessage', (msg) =>{
-        io.emit('message', formatMessage('USER', msg))
+        const user = getCurrentUser(socket.id)
+        io.to(user.room).emit('message', formatMessage(user.username, msg))
     })
 
 
     //runs when client disconnects
     socket.on('disconnect', ()=>{
-        io.emit('message',formatMessage(botName, 'A user has left the chat'))
+        const user = userLeave(socket.id)
+
+        if(user){
+            io.to(user.room).emit('message',formatMessage(botName, `${user.username} has left the chat`))
+
+            io.to(user.room).emit('roomUsers',{
+                room: user.room,
+                users: getRoomUsers(user.room)
+            })
+
+        }
+
     });
 
 })
--- utils/users.js
+++ utils/users.js
@@ -3,7 +3,7 @@ const users = [];
 
 // Join user to chat
 
-fuction userJoin(id, username, room){
+function userJoin(id, username, room){
     const user = {id, username, room};
 
     users.push(user)
@@ -17,7 +17,23 @@ function getCurrentUser(id){
     return users.find(user => user.id == id)
 }
 
+// User leaves chat
+function userLeave(id){
+    const index = users.findIndex(user => user.id === id)
+
+    if(index !== -1){
+        return users.splice(index, 1)[0]
+    }
+}
+
+// Get room users
+function getRoomUsers(room){
+    return users.filter(user => user.room === room)
+}
+
 module.exports = {
     userJoin,
-    getCurrentUser
+    getCurrentUser,
+    userLeave, 
+    getRoomUsers
 }
